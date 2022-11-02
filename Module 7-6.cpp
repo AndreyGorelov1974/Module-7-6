ffd5396e9b60a7ef270bd313d8ee5c10dcc5aa88
Andrey Gorelov <shorkun1974@yandex.ru>
2022-11-02 21:11:57 +0300

    удачный запуск

--- Module 7-6.cpp
+++ Module 7-6.cpp
@@ -18,15 +18,19 @@
 Вывод восклицательных знаков находится посередине, когда это возможно, и со сдвигом на один символ в остальных случаях.*/
 
 #include <iostream>
+#include <Windows.h>
 
 int main()
 {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
 	int maxWidth = 50;
 
 	std::cout << "Введите общую ширину колонтитула: ";
 	int widthFooter;
 	std::cin >> widthFooter;
-	while (widthFooter < 0 && widthFooter >= maxWidth) {
+	while (widthFooter < 0 || widthFooter > maxWidth) {
 		std::cout << "Ширина колонтитула должна быть в пределах от 0 до " << maxWidth << ". Введите снова: ";
 		std::cin >> widthFooter;
 	}
@@ -34,18 +38,20 @@ int main()
 	std::cout << "Введите количество восклицательных знаков: ";
 	int nuberExclamation;
 	std::cin >> nuberExclamation;
-	while (nuberExclamation < 0 && nuberExclamation >= 50) {
+	while (nuberExclamation < 0 || nuberExclamation > maxWidth) {
 		std::cout << "Количество восклицательных знаков должно быть в пределах от 0 до " << maxWidth << ". Введите снова: ";
 		std::cin >> nuberExclamation;
 	}
 
+	int beginPositionExclamation = (widthFooter / 2) - (nuberExclamation / 2);
 
-	for (int y = 1; y <= height; y++) {
-		for (int x = 1; x <= width; x++) {
-			if (x == 1 || x == width) std::cout << "|";
-			else if (y == 1 || y == height) std::cout << "-";
-			else std::cout << " ";
-		}
-		std::cout << std::endl;
-	}
+	//Выводи левую половину колотитула
+	for (int i = 0; i < beginPositionExclamation; i++) std::cout << "~";
+
+	//Выводим восклицательные знаки
+	for (int i = 0; i < nuberExclamation; i++) std::cout << "!";
+
+	//Выводи правую половину колотитула
+	for (int i = beginPositionExclamation + nuberExclamation + 1; i <= widthFooter; i++) std::cout << "~";
+
 }
